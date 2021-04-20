52478e7f88314d63d96cc31a617cf4a71e05daad
ndeely <nialldeely@gmail.com>
2021-04-20 10:58:42 +0100

    cast event target to node

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -22,8 +22,8 @@ export class HeaderComponent implements OnInit {
 
     window.addEventListener('click', e => {
       if (!document.getElementsByClassName('navbar-collapse')[0].classList.contains('collapse') &&
-        !document.getElementsByClassName('navbar-collapse')[0].contains(e.target) &&
-        (!document.getElementsByClassName('hamburger')[0].contains(e.target))) {
+        !document.getElementsByClassName('navbar-collapse')[0].contains(e.target as Node) &&
+        (!document.getElementsByClassName('hamburger')[0].contains(e.target as Node))) {
         document.getElementsByClassName('navbar-collapse')[0].classList.add('collapse');
       }
     });
