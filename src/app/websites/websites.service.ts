92f2d3f3cea3842305a390f404993508cbaa8b7b
ndeely <nialldeely@gmail.com>
2021-04-21 10:57:31 +0100

    rearranged sites, changed icon

--- src/app/websites/websites.service.ts
+++ src/app/websites/websites.service.ts
@@ -8,31 +8,25 @@ export class WebsitesService {
   private websites: Website[] = [
     new Website(
       0,
-      'WordPress',
-      '<p>WordPress is a free and open-source content management system (CMS) written in PHP and paired ' +
-      'with a MySQL or MariaDB database. WordPress is one of the most popular CMSs available and is used by ' +
-      'millions of websites globally.</p>' +
-      '<p>I have worked with WordPress as a Web Developer for over 3 years, and have created custom templates ' +
-      'and plugins to use this CMS to it\'s full potential.</p>' +
-      '<p>Below, you can find a link to an example website I have made using WordPress.</p>',
-      'assets/images/wordpress.png',
-      'www.nialldeely.com/wp'
+      'Ruby on Rails',
+      '<p>Ruby On Rails is a popular model-view-controller (MVC) framework for building websites using the programming ' +
+      'language Ruby.</p>' +
+      '<p>I began using Ruby on Rails in 2020 during my Computer Science college course. While I was not familiar ' +
+      'with the programming language Ruby at the time, I picked it up very quickly due to my experience with other object-oriented ' +
+      'programming languages such as Java, C# and Python.</p>' +
+      '<p>I quickly fell in love with the MVC framework approach to building websites, and have continued to develop my knowledge ' +
+      'of Ruby on Rails after finishing the college course. I have also branched into several other MVC frameworks which are ' +
+      'discussed on this website.</p>' +
+      '<p>Below, you can find a link to an example social media website I have made using Ruby on Rails. This website is a little ' +
+      'different from the others discussed, as it is hosted externally on Heroku due to the limits of my hosting provider for ' +
+      'this website. Heroku hosts this website for free, which I appreciate immensely, but due to this the website does have ' +
+      'some limitations. It does not allow me to store user images or videos to limit the server space that I use, and it can ' +
+      'be slow to load the site initially.</p>',
+      'assets/images/rubyonrails.png',
+      'guarded-meadow-50667.herokuapp.com'
     ),
     new Website(
       1,
-      'Laravel',
-      '<p>Laravel is a free, open-source PHP web framework intended for the development of web applications following the ' +
-      'model–view–controller (MVC) architectural pattern.</p>' +
-      '<p>While I do not have formal work experience using Laravel, I have begun working with it for projects and have ' +
-      'adapted to it very quickly. This is primarily due to my familiarity with PHP through working with WordPress as a ' +
-      'web developer for several years, and my familiarity with MVC frameworks from working on projects built using other ' +
-      'MVC frameworks such as Ruby on Rails and Angular.</p>' +
-      '<p>Below, you can find a link to an example website I have made using Laravel.</p>',
-      'assets/images/laravel.png',
-      'www.nialldeely.com/laravel'
-    ),
-    new Website(
-      2,
       'Angular',
       '<p>Angular is a is a TypeScript-based open-source web application framework.</p>' +
       '<p>I have recently began working with Angular to develop several single page web applications. You are currently ' +
@@ -53,23 +47,29 @@ export class WebsitesService {
       'www.nialldeely.com/angular'
     ),
     new Website(
+      2,
+      'WordPress',
+      '<p>WordPress is a free and open-source content management system (CMS) written in PHP and paired ' +
+      'with a MySQL or MariaDB database. WordPress is one of the most popular CMSs available and is used by ' +
+      'millions of websites globally.</p>' +
+      '<p>I have worked with WordPress as a Web Developer for over 3 years, and have created custom templates ' +
+      'and plugins to use this CMS to it\'s full potential.</p>' +
+      '<p>Below, you can find a link to an example website I have made using WordPress.</p>',
+      'assets/images/wordpress.png',
+      'www.nialldeely.com/wp'
+    ),
+    new Website(
       3,
-      'Ruby on Rails',
-      '<p>Ruby On Rails is a popular model-view-controller (MVC) framework for building websites using the programming ' +
-      'language Ruby.</p>' +
-      '<p>I began using Ruby on Rails in 2020 during my Computer Science college course. While I was not familiar ' +
-      'with the programming language Ruby at the time, I picked it up very quickly due to my experience with other object-oriented ' +
-      'programming languages such as Java, C# and Python.</p>' +
-      '<p>I quickly fell in love with the MVC framework approach to building websites, and have continued to develop my knowledge ' +
-      'of Ruby on Rails after finishing the college course. I have also branched into several other MVC frameworks which are ' +
-      'discussed on this website.</p>' +
-      '<p>Below, you can find a link to an example social media website I have made using Ruby on Rails. This website is a little ' +
-      'different from the others discussed, as it is hosted externally on Heroku due to the limits of my hosting provider for ' +
-      'this website. Heroku hosts this website for free, which I appreciate immensely, but due to this the website does have ' +
-      'some limitations. It does not allow me to store user images or videos to limit the server space that I use, and it can ' +
-      'be slow to load the site initially.</p>',
-      'assets/images/rubyonrails.png',
-      'guarded-meadow-50667.herokuapp.com'
+      'Laravel',
+      '<p>Laravel is a free, open-source PHP web framework intended for the development of web applications following the ' +
+      'model–view–controller (MVC) architectural pattern.</p>' +
+      '<p>While I do not have formal work experience using Laravel, I have begun working with it for projects and have ' +
+      'adapted to it very quickly. This is primarily due to my familiarity with PHP through working with WordPress as a ' +
+      'web developer for several years, and my familiarity with MVC frameworks from working on projects built using other ' +
+      'MVC frameworks such as Ruby on Rails and Angular.</p>' +
+      '<p>Below, you can find a link to an example website I have made using Laravel.</p>',
+      'assets/images/laravel.png',
+      'www.nialldeely.com/laravel'
     )
   ];
 
