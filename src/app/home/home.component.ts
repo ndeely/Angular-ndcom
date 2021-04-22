d596cd604e54ce2c23c9a3ed7883b62d1222ceac
ndeely <nialldeely@gmail.com>
2021-04-22 12:23:18 +0100

    homepage redesign

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,8 @@ import {ProjectsService} from '@projects/projects.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  selectedSite: Website = null;
+  selectedProject: Project = null;
   sites: Website[];
   projects: Project[];
 
@@ -19,6 +21,34 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.sites = this.ws.getSites();
     this.projects = this.ps.getProjects();
+  }
+
+  selectSite(id: number) {
+    const list = document.getElementsByClassName('menu')[0]
+      .getElementsByTagName('ul')[0]
+      .getElementsByTagName('li');
+    if (this.selectedSite) {
+      const previousId = this.selectedSite.id;
+      list[previousId].classList.toggle('active');
+    }
+    this.selectedSite = this.sites[id];
+    list[id].classList.toggle('active');
+  }
+
+  selectProject(id: number) {
+    const list = document.getElementsByClassName('menu')[1]
+      .getElementsByTagName('ul')[0]
+      .getElementsByTagName('li');
+    if (this.selectedProject) {
+      const previousId = this.selectedProject.id;
+      list[previousId].classList.toggle('active');
+    }
+    this.selectedProject = this.projects[id];
+    list[id].classList.toggle('active');
+  }
+
+  onClick(url: string) {
+    window.location.href = 'https://' + url;
   }
 
 }
