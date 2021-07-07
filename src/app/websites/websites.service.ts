a20123a2e0781c7b08d86ea979d2518975a5090b
Niall Deely <nialldeely@gmail.com>
2021-07-07 15:27:24 +0100

    style and content changes
    - hover transitions added to navigation and homepage menu links
    - footer structure improved for mobile
    - website images changed to slideshow on homepage
    - colors updated on homepage
    - content text updated for websites
    - working fade effect added to slideshow
    - slideshow updates for multiple slideshows on single page
    - container styling removed

--- src/app/websites/websites.service.ts
+++ src/app/websites/websites.service.ts
@@ -10,12 +10,11 @@ export class WebsitesService {
       0,
       'Ruby on Rails',
       `
-<p>Ruby On Rails is a popular model-view-controller (MVC) framework for building websites using the programming language Ruby.</p>
-<p>I began using Ruby on Rails in 2020 during my Computer Science college course. While I was not familiar with the programming language
-Ruby at the time, I picked it up very quickly due to my experience with other object-oriented programming languages such as Java, C# and
-Python.</p>
-<p>I quickly fell in love with the MVC framework approach to building websites, and have continued to develop my knowledge of Ruby on Rails
-after finishing the college course. I have also branched into several other MVC frameworks which are discussed on this website.</p>
+<p>Ruby on Rails, or Rails, is a server-side web application framework written in Ruby under the MIT License. Rails is a
+model–view–controller (MVC) framework, providing default structures for a database, a web service, and web pages.</p>
+<p>I have worked with Rails to develop several websites. While I was not familiar with Ruby when I began using Rails, I picked it up very
+quickly due to my familiarity with other object-oriented languages such as Java, Python and C#. My work with Rails was primarily done using
+MySQL for the database, but I have also worked with it using PostgreSQL.</p>
 <p>Below, you can find a link to an example social media website I have made using Ruby on Rails.</p>
 <p class="info">This website is hosted externally on Heroku due to the limits of my hosting plan for this website. Heroku hosts this
 website for free which I appreciate immensely, but due to this, it does have some limitations. It does not allow me to store user images or
@@ -29,8 +28,8 @@ videos to limit the server space that I use, and it can be slow to load the site
       'Angular',
       `
 <p>Angular is a is a TypeScript-based open-source web application framework.</p>
-<p>I have recently began working with Angular to develop several single page web applications. You are currently using one of these
-applications to view this portfolio. The source code for this website is available on GitHub using the link below.</p>
+<p>I have worked with Angular to develop several single page web applications. You are currently using one of these applications to view
+this portfolio. The source code for this website is available on GitHub using the link below.</p>
 <div class="row">
   <div class="col-xs-12 centered">
     <a class="btn btn-primary" href="https://github.com/ndeely/Angular-ndcom" target="_blank">
@@ -38,9 +37,9 @@ applications to view this portfolio. The source code for this website is availab
     </a>
   </div>
 </div>
-<p>I have worked through several tutorials on the Angular framework, building on MVC knowledge gained from studying Ruby on Rails in
-my Computer Science college course. When this course ended in December 2020, I began learning about the viability of other frameworks
-such as Angular and Laravel.</p>
+<p>I have worked through several tutorials on the Angular framework, building on MVC knowledge gained from working with Ruby on Rails. When
+my work with Ruby on Rails concluded, I began working with other frameworks such as Angular and Laravel, using PHP or NodeJS in the back
+end, and working with MySQL, PostgreSQL or MongoDB as the database.</p>
 <p>Below, you can find a link to another example website I have made using Angular.</p>
 `,
       'assets/images/angular.png',
@@ -52,9 +51,9 @@ such as Angular and Laravel.</p>
       `
 <p>Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that can be used to build websites and
 applications.</p>
-<p>I have recently began working with NodeJS to learn the fundamentals using a complete guide to the framework. I am still progressing
-through this course, so I have not added a link to the completed website to this site yet. However, you can visit the GitHub repository for
-this project using the button below.</p>
+<p>I have worked through tutorials with NodeJS to learn the fundamentals using a complete guide to the framework. I have used it to render
+complete content views to the front end, and also to return JSON data to a more developed front end. My work with NodeJS was primarily done
+using MongoDB for the database, but I have also worked with MySQL.</p>
 `,
       'assets/images/nodejs.png',
       ['github.com/ndeely/nodejs-complete-guide']
@@ -63,9 +62,9 @@ this project using the button below.</p>
       3,
       'WordPress',
       `
-<p>WordPress is a free and open-source content management system (CMS) written in PHP and paired with a MySQL or MariaDB database.
+<p>WordPress is a free and open-source content-management system (CMS) written in PHP and paired with a MySQL or MariaDB database.
 WordPress is one of the most popular CMSs available and is used by millions of websites globally.</p>
-<p>I have worked with WordPress as a Web Developer for over 3 years, and have created custom templates and plugins to use this CMS to it's
+<p>I worked with WordPress as a Web Developer for almost 4 years, and have created custom templates and plugins to use this CMS to it's
 full potential.</p>
 <p class="info">I am currently working on a WordPress website to showcase my knowledge of this CMS.</p>
 `,
@@ -79,8 +78,8 @@ full potential.</p>
 <p>Laravel is a free, open-source PHP web framework intended for the development of web applications following the model–view–controller
 (MVC) architectural pattern.</p>
 <p>While I do not have formal work experience using Laravel, I have begun working with it for projects and have adapted to it very quickly.
-This is primarily due to my familiarity with PHP through working with WordPress as a web developer for several years, and my familiarity
-with MVC frameworks from working on projects built using other MVC frameworks such as Ruby on Rails and Angular.</p>
+This is primarily due to my familiarity with PHP through working with WordPress and a PHP back end as a web developer for almost 4 years,
+and my familiarity with MVC frameworks from working on projects built using other MVC frameworks such as Rails and Angular.</p>
 <p class="info">I am currently working on a Laravel website to showcase my knowledge of this framework.</p>
 `,
       'assets/images/laravel.png',
--- src/assets/js/slideshow.js
+++ src/assets/js/slideshow.js
@@ -1,16 +1,28 @@
-var slideIndex = 0;
+let slideIndex = 0;
 setTimeout(carousel, 1000);
 
 function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (x.length !== 0) {
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+  let current;
+  const slides = document.getElementsByClassName("mySlides");
+  const slides2 = document.getElementsByClassName("mySlides2");
+
+  if (slides.length !== 0) {
+    for (current = 0; current < slides.length; current++) {
+      slides[current].classList.remove('active');
     }
     slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex - 1].classList.add('active');
   }
+
+  if (slides2.length !== 0) {
+    for (current = 0; current < slides2.length; current++) {
+      slides2[current].classList.remove('active');
+    }
+    slideIndex++;
+    if (slideIndex > slides2.length) {slideIndex = 1}
+    slides2[slideIndex - 1].classList.add('active');
+  }
+
   setTimeout(carousel, 3000); // Change image every 3 seconds
 }
